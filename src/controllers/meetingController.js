bb21b148180ae3875ef3c88db568ffe6afbafa32
shuzikicebot581 <eiainehaley@hotmail.com>
2025-08-28 21:37:50 -0500

    feat: initial commit for src/controllers/meetingController.js

---
+++ src/controllers/meetingController.js
@@ -0,0 +1,17 @@
+// Controlador para gestionar la lógica de reuniones
+
+exports.createMeeting = async (req, res) => {
+    // Implementación de la creación de reuniones
+};
+
+exports.getMeetings = async (req, res) => {
+    // Implementación de la obtención de reuniones
+};
+
+exports.updateMeeting = async (req, res) => {
+    // Implementación de la edición de reuniones
+};
+
+exports.deleteMeeting = async (req, res) => {
+    // Implementación de la eliminación de reuniones
+};
