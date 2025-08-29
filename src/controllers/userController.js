383d84a387841eedeef70fd45ecad5d7191f3b3f
shuzikicebot581 <eiainehaley@hotmail.com>
2025-08-28 21:37:51 -0500

    feat: initial commit for src/controllers/userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+// Controlador para la gestión de usuarios
+
+exports.registerUser = async (req, res) => {
+    // Implementación del registro de usuarios
+};
+
+exports.loginUser = async (req, res) => {
+    // Implementación de la autenticación de usuarios
+};
